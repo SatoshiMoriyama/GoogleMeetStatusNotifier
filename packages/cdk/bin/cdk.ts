20fdd8f00f7e15c50f1c5bec81b14dc5f33f8191
SatoshiMoriyama <satoshi.moriyama@serverworks.co.jp>
2025-11-10 05:41:04 +0900

    NAG追加

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
 import * as cdk from "aws-cdk-lib";
+import { AwsSolutionsChecks } from "cdk-nag";
 import { ApplicationStage } from "../lib/stage/application-stage";
 
 const app = new cdk.App();
 
-new ApplicationStage(app, "Dev", {
+const stage = new ApplicationStage(app, "Dev", {
   alexaClientId: process.env.ALEXA_CLIENT_ID || "",
   alexaClientSecret: process.env.ALEXA_CLIENT_SECRET || "",
   alexaSkillId: process.env.ALEXA_SKILL_ID || "",
   env: { region: "ap-northeast-1" },
 });
+
+// CDK Nag チェックを適用
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
